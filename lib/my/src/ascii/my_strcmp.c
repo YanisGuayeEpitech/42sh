07d28ea5242d2256853031a3fb9e34afc535a645
Yanis Guaye <yanis.guaye@epitech.eu>
2021-01-15 19:07:03 +0100

    libmy: ascii module: eliminate unnecessary branches

--- lib/my/src/ascii/my_strcmp.c
+++ lib/my/src/ascii/my_strcmp.c
@@ -13,7 +13,7 @@ MY_API int my_strcmp(char const *s1, char const *s2)
         return -1;
     if (!s2 && s1)
         return 1;
-    if (!s1 && !s2)
+    if (!s1)
         return 0;
     while (*s1 && *s2 && *s1 == *s2) {
         ++s1;
--- lib/my/src/ascii/my_stricmp.c
+++ lib/my/src/ascii/my_stricmp.c
@@ -13,7 +13,7 @@ MY_API int my_stricmp(char const *s1, char const *s2)
         return -1;
     if (!s2 && s1)
         return 1;
-    if (!s1 && !s2)
+    if (!s1)
         return 0;
     while (*s1 && *s2 && my_tolower(*s1) == my_tolower(*s2)) {
         ++s1;
