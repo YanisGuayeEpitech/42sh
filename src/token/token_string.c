9f29acd395da8093db97a17046222a152bdc19ea
Andreas Leroux <andreas.leroux@epitech.eu>
2021-05-17 20:41:51 +0200

    Add inhibitors on quoted strings

--- src/token/token_string.c
+++ src/token/token_string.c
@@ -5,33 +5,9 @@
 ** Parses string tokens
 */
 
-#include <libmy/memory/alloc.h>
-#include <libmy/printf.h>
 #include <stdlib.h>
 
 #include "token.h"
-
-int sh_token_quoted_string(
-    sh_token_stream_t *stream, sh_token_t *token, char term)
-{
-    char *end = my_memchr(
-        SH_STREAM_CURRENT(stream, 0), term, SH_STREAM_CURRENT_LEN(stream));
-    size_t len;
-
-    if (end == NULL) {
-        my_fprintf(MY_STDERR, "Unmatched '%c'\n", end);
-        my_flush_stderr();
-        return 1;
-    }
-    len = (end - SH_STREAM_CURRENT(stream, 0)) + 1;
-    *token = (sh_token_t){.token_type = SH_TOKEN_STRING,
-        .str = my_memdup(SH_STREAM_CURRENT(stream, 0), sizeof(char) * len)};
-    if (token->str == NULL)
-        return 1;
-    token->str[len - 1] = '\0';
-    stream->line_pos += len;
-    return 0;
-}
 
 static ssize_t sh_token_unquoted_string_copy(
     sh_token_stream_t *stream, my_vec_t *str)
