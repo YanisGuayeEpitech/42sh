32a53e1c6c3c75c653e92b3088ca642746778b59
Yanis Guaye <yanis.guaye@epitech.eu>
2021-05-20 23:30:17 +0200

    Add ability to load multiple integration test suites

--- tests/integration/tester.sh
+++ tests/integration/tester.sh
@@ -3,6 +3,11 @@
 MYSHELL="$PWD/../../42sh"
 REFER="/bin/tcsh -f"
 
+# Add the test suite files below one per line
+SUITES=(
+  'test_basic.toml'
+)
+
 # Undefines any previously defined bash function
 for i in `env | grep BASH_FUNC_ | cut -d= -f1`; do
   f=`echo $i | /usr/bin/sed s/BASH_FUNC_//g | /usr/bin/sed s/%%//g`
@@ -38,24 +43,24 @@ if test -t 1; then
 fi
 
 # Gets the value of string properties with the first argument as names
-get_string_property() {
+function get_string_property {
   $SED -ne "s/^$1\s*=\s*\(.*\)\s*$/\1/p" | $SED -e "s/^\(\"\(.*\)\"\)\|\('\(.*\)'\)$/\2\4/"
 }
 
 # Displays the data of the test $id
-display_test() {
+function display_test {
   id=$1
   $CAT test_basic.toml | $GREP -A1000 "\[$id\]" | $GREP -B1000 "^\[$id-END\]" | $GREP -v "^\[.*\]"
 }
 
-run_script() {
+function run_script {
   SC="$1"
   echo "$SC" > /tmp/.tmp.$$
   . /tmp/.tmp.$$
   $RM -f /tmp/.tmp.$$
 }
 
-prepare_test() {
+function prepare_test {
   local testfn="/tmp/.tester.$$"
   local runnerfn="/tmp/.runner.$$"
   local refoutfn="/tmp/.refer.$$"
@@ -80,15 +85,16 @@ prepare_test() {
 }
 
 # Parameters:
-# 1 - test file contents
-# 2 - test suite
+# 1 - suite file contents
+# 2 - suite name
 # 3 - test id
-run_test() {
-  local test_file=$1
+# Returns 0 if test passed, 1 otherwise
+function run_test {
+  local suite_contents=$1
   local suite=$2
   local id=$3
 
-  local properties=`echo "$test_file" | $SED -ne  "/^\[$id\]\$/,/^\[.*\]$/{/^\[.*\]$/d;p}"`
+  local properties=`echo "$suite_contents" | $SED -ne  "/^\[$id\]\$/,/^\[.*\]$/{/^\[.*\]$/d;p}"`
 
   # extract 'tests' multiline property first...
   TESTS=`echo "$properties" | $SED -ne '/^tests\s*=\s*"""$/,/^"""$/{/^tests\s*=\s*"""$/d;/^"""$/d;p}'`
@@ -126,18 +132,28 @@ run_test() {
   fi
 }
 
-run_test_suite() {
-  local test_file=`$CAT $1`
-  local suite_properties=`echo "$test_file" | $SED -ne '1,/^\[.*\]$/{/^\[.*\]$/d;p}'`
+# Parameters:
+# 1 - suite file path
+# Returns 0 if all tests passed, 1 otherwise
+function run_test_suite {
+  local test_file="$1"
+  
+  if [ ! -f "$test_file" ]; then
+    echo "No such test suite at path $test_file" >&2
+    return 1
+  fi
+
+  local suite_contents=`$CAT $test_file`
+  local suite_properties=`echo "$suite_contents" | $SED -ne '1,/^\[.*\]$/{/^\[.*\]$/d;p}'`
   local suite=`echo "$suite_properties" | get_string_property 'suite'`
   local tested=0
   local passing=0
   local failing=0
 
-  for id in `echo "$test_file" | $SED -ne '/\[.*\]/!d;s/\[\(.*\)\]/\1/p' "$1"`; do
+  for id in `echo "$suite_contents" | $SED -ne '/\[.*\]/!d;s/\[\(.*\)\]/\1/p' "$1"`; do
     local path_backup=$PATH
 
-    if run_test "$test_file" "$suite" "$id" 1; then
+    if run_test "$suite_contents" "$suite" "$id" 1; then
       passing=$(($passing + 1))
     else
       failing=$(($failing + 1))
@@ -146,16 +162,18 @@ run_test_suite() {
     export PATH=$path_backup
   done
   echo -e "[${BLUE}====${NC}] ${BOLD}Synthesis: Tested:${NC} ${BLUE}${tested}${NC} | ${BOLD}Passing:${NC} ${GREEN}${passing}${NC} | ${BOLD}Failing:${NC} ${RED}${failing}${NC}"
+  [ $failing -eq 0 ]
 }
-
-if [ ! -f test_basic.toml ]; then
-  echo "No test_basic.toml file. Please read README.ME" >&2
-  exit 1
-fi
 
 if [ ! -f $MYSHELL ]; then
   echo "$MYSHELL not found" >&2
   exit 1
 fi
 
-run_test_suite 'test_basic.toml'
+code=0
+for test_file in ${SUITES[@]}; do
+  run_test_suite "$test_file"
+  ret=$?
+  [ ! $ret -eq 0 ] && code=$ret
+done
+exit $code
